b98608bf6b2e3150ff7eb030d126a74b5eca974e
surajmahangade <surajmahangade99@gmail.com>
2021-01-27 02:35:04 +0530

    Karnataka H F done working on T

--- states/Karnataka.py
+++ states/Karnataka.py
@@ -81,97 +81,86 @@ def Karnataka(data,contractor_name,contractor_address,filelocation,month,year,re
         data_once_per_sheet=templates.get_diff_data_once_persheet_peremployee(data_formF,{"A4":employee_code_column,"A5":"Date Joined",
                                                         "A6":employee_name_column,"A7":fathers_name_column,"A8":"permanent_address"})
         
+        data_with_attendance = templates.get_from_to_dates_attendance(data_formF,"PL",sno_column="S.no")
         templates.create_attendance_form_per_employee(filename="Form F register of leave with wages.xlsx",sheet_name="Sheet1",
                                     start_row=13,start_column=1,
-                                    data=data_formF,columns=columns,data_once_per_sheet=data_once_per_sheet,per_employee_diff_data=True)
-
+                                    data_with_attendance=data_with_attendance,columns=columns,data_once_per_sheet=data_once_per_sheet,per_employee_diff_data=True)
 
     
-    def create_form_A():
-
-        formAfilepath = os.path.join(karnatakafilespath,'FormA.xlsx')
-        formAfile = load_workbook(filename=formAfilepath)
-        logging.info('Form A file has sheet: '+str(formAfile.sheetnames))
+    def Form_H():
+        leave_file_data=data[[employee_code_column,employee_name_column,"Leave Type","Opening",
+                                                                "Monthly Increment","Leave Accrued","Used","Encash","Closing"]]
 
         
+        data_formH = data.drop_duplicates(subset=['Employee Code']).copy()
+
+        columns=['S.no','month_start','month_end',"Days Paid","Monthly Increment","open_balance","from","to","numdays","colsing_bal",
+                                    "Date_of_payement_fixed_monthly","remarks","slno","year","of_credit","availed","Balance"]
+
+        for employee_name_leave_file in data_formH[employee_name_column]:
+            #opening
+            emp_details=leave_file_data.loc[leave_file_data[employee_name_column]==employee_name_leave_file,:]
+            opening_pl=emp_details["Opening"].loc[emp_details["Leave Type"]=="PL"].astype(float)
+            opening_cl=emp_details["Opening"].loc[emp_details["Leave Type"]=="CL"].astype(float)
+            opening_sl=emp_details["Opening"].loc[emp_details["Leave Type"]=="SL"].astype(float)
+            prev_bal=opening_pl.add(opening_cl.add(opening_sl,fill_value=0), fill_value=0).sum()
+            
+            data_formH.loc[data_formH[employee_name_column]==employee_name_leave_file,"open_balance"]=prev_bal
+
+            #closing
+            Closing_pl=emp_details["Closing"].loc[emp_details["Leave Type"]=="PL"].astype(float)
+            Closing_cl=emp_details["Closing"].loc[emp_details["Leave Type"]=="CL"].astype(float)
+            Closing_sl=emp_details["Closing"].loc[emp_details["Leave Type"]=="SL"].astype(float)
+            closing=Closing_cl.add(Closing_pl.add(Closing_sl,fill_value=0), fill_value=0).sum()
+            
+            data_formH.loc[data_formH[employee_name_column]==employee_name_leave_file,"colsing_bal"]=closing
+
+        start_month = datetime.date(year,month_num,1)
+        end_month = datetime.date(year,month_num,calendar.monthrange(year,month_num)[1])
+        data_formH['S.no'] = list(range(1,len(data_formH)+1))
+        data_formH['month_start']=start_month
+        data_formH['month_end']=end_month
+        data_formH["Date_of_payement_fixed_monthly"]=templates.combine_columns_of_dataframe(data_formH,
+                                                            [date_of_payment_column,fix_monthly_gross_column])
+        data_formH["remarks"]=""
+        data_formH[["slno","year","of_credit","availed","Balance"]]="---"
+        data_formH["permanent_address"]=templates.combine_columns_of_dataframe(data_formH,["Permanent Address 1","Permanent Address 2",
+                                                                "Permanent Address 3","Permanent Address 4"]," ")      
+        
+        data_once_per_sheet=templates.get_diff_data_once_persheet_peremployee(data_formH,{"A4":employee_code_column,"A5":"Date Joined",
+                                                        "A6":employee_name_column,"A7":fathers_name_column,"A8":"permanent_address"})
+        data_with_attendance = templates.get_from_to_dates_attendance(data_formH,"PL",sno_column="S.no")
+        templates.create_attendance_form_per_employee(filename="Form H leave with wages.xlsx",sheet_name="Sheet1",
+                                    start_row=13,start_column=1,
+                                    data_with_attendance=data_with_attendance,columns=columns,data_once_per_sheet=data_once_per_sheet,per_employee_diff_data=True)
+
+    def Form_T():
         logging.info('create columns which are now available')
+        data_formT = data.copy(deep=True)
+        data_formT=data_formT.drop_duplicates(subset=employee_code_column, keep="last")
 
-        data_formA = data.drop_duplicates(subset=['Employee Code']).copy()
-
-        data_formA.fillna(value=0, inplace=True)
-
-        data_formA['S.no'] = list(range(1,len(data_formA)+1))
-
-        #data_formA["Nationality"] =''
-        #data_formA['education level'] = ''
-        #data_formA['Category address'] = ''
-        #data_formA['type of employment'] = ''
-        #data_formA['lwf'] = ''
-        #data_formA['Service Book No'] = ''
-        data_formA["a"] = ''
-        data_formA["b"] = ''
-        data_formA["c"] = ''
-        formA_columns = ["S.no",'Employee Code','Employee Name','Unit','Location',"Gender","Father's Name",
-                'Date of Birth',"Nationality","Education Level",'Date Joined','Designation','Category Address',"Type of Employment",
-                'Mobile Tel No.','UAN Number',"PAN Number","ESIC Number","LWF EE","Aadhar Number","Bank A/c Number",
-                "Bank Name","Account Code","P","L","Service Book No","Date Left","Reason for Leaving","Identification mark","a","b","c"]
-        formA_data = data_formA[formA_columns]
+        columns=['S.no',employee_code_column,employee_name_column,fathers_name_column,gender_column,"Designation"
+                                        department_column,address_column,"Date Joined","ESIC Number",'PF Number',"VDA","Days Paid",
+                                        'Total\r\nOT Hrs','basic_da','Earned Basic','HRA','Bonus','Special Allowance','Overtime',
+                                        'NFH','maternity','Telephone Reimb','Bonus','Fuel Reimb','Prof Dev Reimb', 'Corp Attire Reimb',
+                                        'CCA','Others','subsistence','Leave Encashment',
 
 
-        formAsheet = formAfile['FORM A']
 
-        formAsheet.sheet_properties.pageSetUpPr.fitToPage = True
+                                        "name&date_of_offence","cause_against_fine",
+                                        fix_monthly_gross_column,date_of_payment_column,"Date of Fine","remarks"]
 
-        logging.info('data for form A is ready')
-
-        
-        rows = dataframe_to_rows(formA_data, index=False, header=False)
-
-        logging.info('rows taken out from data')
-
-        data_once_per_sheet={'A5':data_formI[company_name_column].unique()[0],'A6':str(month)+" "+str(year)}
-        templates.create_basic_form(filename='FormA.xlsx',
-                                    sheet_name='FORM A',all_employee_data=formA_data,start_row=13,start_column=1,
+        data_formT['S.no'] = list(range(1,len(data_formT)+1))
+        data_formT[["name&date_of_offence","cause_against_fine",fix_monthly_gross_column,date_of_payment_column,"Date of Fine","remarks"]]="NIL"
+        formI_data=data_formT[columns]
+        data_once_per_sheet={'A8':str(data_formT['Unit'].unique()[0])+","+str(data_formT[address_column].unique()[0]),
+                                'A9':str(data_formT['Unit'].unique()[0])+","+str(data_formT[address_column].unique()[0]),
+                                'A10':contractor_name,
+                                'A11':data_formT['Nature of work'].unique()[0]+', '+data_formT['Location'].unique()[0]}
+        templates.create_basic_form(filename='Form T Combine Muster roll cum register of wages.xlsx',
+                                    sheet_name='Sheet1',all_employee_data=formI_data,start_row=8,start_column=1,
                                     data_once_per_sheet=data_once_per_sheet)
-        
 
-        logging.info('')
-
-
-        if str(data_formA['Company Name'].dtype)[0:3] != 'obj':
-            data_formA['Company Name'] = data_formA['Company Name'].astype(str)
-
-        if str(data_formA['Company Address'].dtype)[0:3] != 'obj':
-            data_formA['Company Address'] = data_formA['Company Address'].astype(str)
-
-        if str(data_formA['Contractor_name'].dtype)[0:3] != 'obj':
-            data_formA['Contractor_name'] = data_formA['Contractor_name'].astype(str)
-
-        if str(data_formA['Contractor_Address'].dtype)[0:3] != 'obj':
-            data_formA['Contractor_Address'] = data_formA['Contractor_Address'].astype(str)
-
-        if str(data_formA['Unit'].dtype)[0:3] != 'obj':
-            data_formA['Unit'] = data_formA['Unit'].astype(str)
-
-        if str(data_formA['Branch'].dtype)[0:3] != 'obj':
-            data_formA['Branch'] = data_formA['Branch'].astype(str)
-
-        establishment = formAsheet['L6'].value
-        if data_formA['PE_or_contract'].unique()[0] == 'PE':
-            L6_data = establishment+' '+data_formA['Company Name'].unique()[0] +', '+data_formA['Company Address'].unique()[0]  
-        else:    
-            L6_data = establishment+' '+data_formA['Contractor_name'].unique()[0]+', '+data_formA['Contractor_Address'].unique()[0]
-        formAsheet['L6'] = L6_data
-
-
-        company = formAsheet['A10'].value
-        A10_data = company+' '+data_formA['Unit'].unique()[0]+', '+data_formA['Branch'].unique()[0]
-        formAsheet['A10'] = A10_data
-
-        
-        formAfinalfile = os.path.join(filelocation,'FormA.xlsx')
-        logging.info('Form A file is' +str(formAfinalfile))
-        formAfile.save(filename=formAfinalfile)
         
 
     def create_form_B():
@@ -1171,6 +1160,8 @@ def Karnataka(data,contractor_name,contractor_address,filelocation,month,year,re
             
     try:
         Form_F()
+        Form_H()
+
         return
         create_form_A()
         create_form_B()
